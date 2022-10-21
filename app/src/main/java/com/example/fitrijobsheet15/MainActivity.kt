18d17e30637f8247c5e7b2aeacdc9c71df736bd1
Your Name <zahrotun@gmail.com>
2022-10-21 11:26:51 +0700

    first commit

---
+++ app/src/main/java/com/example/fitrijobsheet15/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.fitrijobsheet15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnnama = findViewById<Button>(R.id.nama)
+        val btnkelas = findViewById<Button>(R.id.kelas)
+        val btnnis = findViewById<Button>(R.id.nis)
+
+        btnnama.setOnClickListener {
+            Toast.makeText(this, "FITRI RAHMAWATI", Toast.LENGTH_SHORT).show()
+        }
+        btnkelas.setOnClickListener {
+            Toast.makeText(this, "XI PPLG 2", Toast.LENGTH_SHORT).show()
+        }
+        btnnis.setOnClickListener {
+            Toast.makeText(this, "2992", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
